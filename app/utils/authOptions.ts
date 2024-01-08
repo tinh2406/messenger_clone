2f39a74176d0cbc10d259cbf2ea5510d160f7348
tinh2406 <nguyenquoctinh24062002@gmail.com>
2024-01-09 00:52:21 +0700

    Fix

--- app/utils/authOptions.ts
+++ app/utils/authOptions.ts
@@ -18,6 +18,7 @@ export const authOptions: AuthOptions = {
         password: { label: "password", type: "password" },
       },
       async authorize(credentials) {
+        console.log("User:::credentials",credentials);
         if (!credentials?.email || !credentials?.password) {
           throw new Error("Invalid credentials");
         }
